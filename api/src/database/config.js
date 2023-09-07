fc380008f03f2fdd90b4b9c479f9af5d844087d9
Filipe <filipe-ferreira-1@hotmail.com>
2023-09-06 20:15:24 -0400

    first commit

---
+++ api/src/database/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dialect: 'postgres',
+    database: 'postgres',
+    username: 'postgres',
+    password: '123',
+    host: 'localhost',
+    port: 5432,
+    logging: false
+};
